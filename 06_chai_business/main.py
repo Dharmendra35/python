b703cd463fbfd60fa6c257d10c12a6f4a8da1717
Dharmendra35 <dk292767@gmail.com>
2025-12-10 12:14:49 +0530

    learned about import in python

---
+++ 06_chai_business/main.py
@@ -0,0 +1,10 @@
+# import recipies.flavors
+# print(recipies.flavors.ginger_chai())
+
+# another way to import
+# from recipies.flavors import elachai_chai, ginger_chai
+# print(ginger_chai())
+
+from .recipies.flavors import ginger_chai
+
+
---
+++ 06_chai_business/recipies/flavors.py
@@ -0,0 +1,5 @@
+def elachai_chai():
+  return "Elachai chai is ready"
+
+def ginger_chai():
+  return "ginger chai is ready"
